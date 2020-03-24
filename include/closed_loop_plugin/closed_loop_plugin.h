2219936a35ffa41753d9975d3c97ac276aa6bb42
nyxrobotics <takaaki.numai01@g.softbank.co.jp>
2020-03-24 16:54:02 +0900

    add catkin_clang_format dependency
    see here : https://github.com/nyxrobotics/catkin_clang_format

--- include/closed_loop_plugin/closed_loop_plugin.h
+++ include/closed_loop_plugin/closed_loop_plugin.h
@@ -12,56 +12,53 @@
 
 // Gazebo includes
 #include <gazebo/common/Plugin.hh>
+#include <gazebo/common/common.hh>
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
-#include <gazebo/common/common.hh>
 //
 #include <ignition/math/Pose3.hh>
 #include <vector>
 
-namespace gazebo
-{
-  class ClosedLoopPlugin : public ModelPlugin
-  {
-    public:
-      ClosedLoopPlugin();
-      ~ClosedLoopPlugin();
+namespace gazebo {
+class ClosedLoopPlugin : public ModelPlugin {
+public:
+  ClosedLoopPlugin();
+  ~ClosedLoopPlugin();
 
-      void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);
-      void UpdateChild();
+  void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);
+  void UpdateChild();
 
-    private:
-      std::vector<float> Convert_to_float(const std::vector<std::string>& subject);
-      std::vector<std::string> Split_String(const std::string& subject);
+private:
+  std::vector<float> Convert_to_float(const std::vector<std::string> &subject);
+  std::vector<std::string> Split_String(const std::string &subject);
 
-      // Parameters
-      std::string joint_name_, child_name_, parent_name_;
-      std::string rotation_;
-      std::string position_;
+  // Parameters
+  std::string joint_name_, child_name_, parent_name_;
+  std::string rotation_;
+  std::string position_;
 
-      sdf::ElementPtr insert_sdf_;
+  sdf::ElementPtr insert_sdf_;
 
-      bool kill_sim;
+  bool kill_sim;
 
-      // Pointers to the joints
-      physics::JointPtr joint_;
+  // Pointers to the joints
+  physics::JointPtr joint_;
 
-      // Pointers to the links
-      physics::LinkPtr parent_, child_;
+  // Pointers to the links
+  physics::LinkPtr parent_, child_;
 
-      // Pointer to the model
-      physics::ModelPtr model_;
+  // Pointer to the model
+  physics::ModelPtr model_;
 
-      // Pointer to the world
-      physics::WorldPtr world_;
+  // Pointer to the world
+  physics::WorldPtr world_;
 
-      // Pointer to the physics_
-      physics::PhysicsEnginePtr physics_;
+  // Pointer to the physics_
+  physics::PhysicsEnginePtr physics_;
 
-      // Pointer to the update event connection
-      event::ConnectionPtr updateConnection;
-
-  };
-}
+  // Pointer to the update event connection
+  event::ConnectionPtr updateConnection;
+};
+} // namespace gazebo
 
 #endif
